24af6ebe0cd7594a39972914499899b5f38f6a34
Ashray Shah <geektraindev@gmail.com>
2023-03-23 21:50:15 -0500

    Initial Counter Bot

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+] });
+
+const listenChannel = '1088654010870931527';
+let number = 1;
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, message => {
+	if (message.channelId != listenChannel) {
+		return;
+	}
+
+	if (message.content == number) {
+		message.react('✅');
+		number++;
+	}
+	else {
+		number = 1;
+		message.reply('Wrong number! Next number is **1**');
+	}
+});
+
+client.login(token);
